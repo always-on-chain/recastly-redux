3dcce427c1dc341e2043934ce946ad7d9c18105d
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-04-04 15:30:33 -0700

    got up to store

--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -1,7 +1,16 @@
 import Redux from 'redux';
 
+const sampleData = [];
+
 var videoListReducer = (state = sampleData, action) => {
   //TODO: define a reducer for the videoList field of our state.
+  console.log(action)
+  switch (action.type) {
+    case 'CHANGE_VIDEO_LIST':
+        return action.videos;
+    default:
+        return state;
+  }
 };
 
 export default videoListReducer;
